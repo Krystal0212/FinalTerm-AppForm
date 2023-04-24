9ea89210f14812bd87cd83a5e37c828a6bcec645
Krystal0212 <ntu4318@gmail.com>
2023-04-25 01:54:14 +0700

    Upload new version

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FormApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            resources.ApplyResources(this.txtUsername, "txtUsername");
+            this.txtUsername.Name = "txtUsername";
+            // 
+            // txtPass
+            // 
+            resources.ApplyResources(this.txtPass, "txtPass");
+            this.txtPass.Name = "txtPass";
+            // 
+            // Username
+            // 
+            resources.ApplyResources(this.Username, "Username");
+            this.Username.Name = "Username";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // bLogin
+            // 
+            resources.ApplyResources(this.bLogin, "bLogin");
+            this.bLogin.Name = "bLogin";
+            this.bLogin.UseVisualStyleBackColor = true;
+            this.bLogin.Click += new System.EventHandler(this.bLogin_Click);
+            // 
+            // bCancel
+            // 
+            resources.ApplyResources(this.bCancel, "bCancel");
+            this.bCancel.Name = "bCancel";
+            this.bCancel.UseVisualStyleBackColor = true;
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
+            // 
+            // LoginForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ControlBox = false;
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtUsername;
+        private TextBox txtPass;
+        private Label Username;
+        private Label label1;
+        private Label label2;
+        private Button bLogin;
+        private Button bCancel;
+    }
+}
